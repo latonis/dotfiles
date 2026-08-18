41f999af3828c2fef0413d0485151ef9f2917deb
Jacob Latonis <jlatonis@me.com>
2026-08-18 13:50:11 -0700

    feat: emacs git ignore and init for emacs

--- config/emacs/init.el
+++ config/emacs/init.el
@@ -1,2 +1,30 @@
+(require 'package)
+
 (add-hook 'window-setup-hook #'global-display-line-numbers-mode)
 (menu-bar-mode -1)
+
+; need melpa for most user packages
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(use-package elixir-mode
+  :ensure t)
+
+(use-package lsp-mode
+  :ensure t
+  :commands lsp
+  :hook (elixir-mode . lsp)
+  :init
+  )
+(use-package lsp-ui)
